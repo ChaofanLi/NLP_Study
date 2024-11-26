1ef5e11f85a385de554e919a68b8973b7d773a15
ChaofanLi <162019957+ChaofanLi@users.noreply.github.com>
2024-11-26 23:06:08 +0800

    Add files via upload

---
+++ 文本匹配/sentence_match_as_sentence_encoder/config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": r"G:\NLP\Code\NLP_Code\7_文本匹配任务\sentence_match_as_sentence_encoder\model_output",
+    "schema_path": r"G:\NLP\Code\NLP_Code\7_文本匹配任务\data\schema.json",
+    "train_data_path": r"G:\NLP\Code\NLP_Code\7_文本匹配任务\data\train.json",
+    "valid_data_path": r"G:\NLP\Code\NLP_Code\7_文本匹配任务\data\valid.json",
+    "vocab_path":r"G:\NLP\Code\NLP_Code\7_文本匹配任务\chars.txt",
+    "max_length": 20,
+    "hidden_size": 128,
+    "epoch": 10,
+    "batch_size": 32,
+    "epoch_data_size": 200,     #每轮训练中采样数量
+    "positive_sample_rate":0.5,  #正样本比例
+    "optimizer": "adam",
+    "learning_rate": 1e-3,
+}
