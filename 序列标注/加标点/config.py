0eb2a0dfcf179e220e4286b6e8983c1333d244a7
ChaofanLi <162019957+ChaofanLi@users.noreply.github.com>
2025-01-02 22:07:11 +0800

    Update config.py

--- 序列标注/加标点/config.py
+++ 序列标注/加标点/config.py
@@ -6,9 +6,9 @@
 
 Config = {
     "model_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\model_output",
-    "schema_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\data\schema.json",
-    "train_data_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\data\train_corpus",
-    "valid_data_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\data\valid_corpus",
+    "schema_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\dataset\schema.json",
+    "train_data_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\dataset\train_corpus",
+    "valid_data_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\dataset\valid_corpus",
     "vocab_path":r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\chars.txt",
     "max_length": 50,
     "hidden_size": 128,
