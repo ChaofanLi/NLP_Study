28626a0927bf13b5011f7df735c8f394ff154bfa
ChaofanLi <162019957+ChaofanLi@users.noreply.github.com>
2025-01-02 22:02:57 +0800

    Add files via upload

---
+++ 序列标注/加标点/config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\model_output",
+    "schema_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\data\schema.json",
+    "train_data_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\data\train_corpus",
+    "valid_data_path": r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\data\valid_corpus",
+    "vocab_path":r"G:\NLP\Code\NLP_Code\8_序列标注任务\添加标点\chars.txt",
+    "max_length": 50,
+    "hidden_size": 128,
+    "epoch": 10,
+    "batch_size": 128,
+    "optimizer": "adam",
+    "learning_rate": 1e-3,
+    "use_crf": False,
+    "class_num": None
+}
